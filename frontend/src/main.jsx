be5d716ff1717087428d7cbe2825281e2ffba00a
midnite-ops <charlesoyims@gmail.com>
2026-06-25 16:09:01 +0100

    installed react-hot-toast,react -router, tailwind and daisy ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from "react-router"
 import { createRoot } from 'react-dom/client'
+import { Toaster } from "react-hot-toast"
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
   </StrictMode>,
 )

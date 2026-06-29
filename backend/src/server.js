3fa57d6c1173d42d818125b5f192e76a4e3595f0
midnite-ops <charlesoyims@gmail.com>
2026-06-29 21:04:37 +0100

    Prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,21 @@ import { connectDb } from "./config/db.js";
 import dotenv from 'dotenv'
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors"
+import path, { dirname } from 'path'
 
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT
+const __dirname = path.resolve()
+console.log(__dirname)
 
-app.use(cors({
-  origin: "http://localhost:5173"
-}))
+if(process.env.NODE_ENV !== 'production'){
+  app.use(cors({
+    origin: "http://localhost:5173"
+  }))
+}
+
 //middleware
 app.use(express.json()) //This middleware will parse JSON bodies: req.body
 
@@ -26,6 +32,19 @@ app.use(rateLimiter)
 })*/
 
 app.use("/api/notes", noteRoutes);
+
+// Serve the React app only in production.
+if (process.env.NODE_ENV === "production") {
+
+  // Serve static files (HTML, CSS, JavaScript, images) from the React build folder.
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // Return index.html for any route not handled by the backend,
+  // allowing React Router to manage client-side navigation.
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 //Always connect to the database before listening for a port
 
